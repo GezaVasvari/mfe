21c48fb4752409e1b53d39f81f1ddddfbd187652
GezaVasvari <vasvari.geza@outlook.com>
2023-04-14 06:43:11 -0700

    bunch of updates

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,4 +1,4 @@
-import { mount } from "marketing/MarketingApp";
+import { mount } from "../../../auth/src/index";
 import React, { useRef, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
