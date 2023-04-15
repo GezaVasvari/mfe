216c9645baf7abd6a73e858014b8f11b6a93f4c5
GezaVasvari <vasvari.geza@outlook.com>
2023-04-15 09:59:21 -0700

    added auth subapp

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -2,7 +2,7 @@ import { mount } from "../../../auth/src/index";
 import React, { useRef, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
 
@@ -13,6 +13,10 @@ export default () => {
         const { pathname } = history.location;
         if (pathname !== nextPathname) history.push(nextPathname);
       },
+      onSignIn
+      // onSignIn: () => {
+      //   onSignIn();
+      // },
     });
 
     history.listen(onParentNavigate);
